64a8c706091658bd6579d1d3881eecd0b188201e
Addy <aclaure@gmail.com>
2021-01-01 00:02:28 -0500

    more housekeeping;

--- src/CryptoUtils.cpp
+++ src/CryptoUtils.cpp
@@ -10,7 +10,6 @@
 #include "CryptoUtils.h"
 
 using namespace std::string_literals;
-using namespace std::string_view_literals;
 
 namespace ash
 {
--- src/CryptoUtils.h
+++ src/CryptoUtils.h
@@ -10,10 +10,6 @@ namespace crypto
 {
 
 std::string SHA256(std::string_view data);
-std::string SHA256HexString(std::string_view data);
-std::string RIPEMD160HexString(std::string_view data);
-
-std::string Base58Encode(CryptoPP::Integer num);
 
 std::string GetPublicKey(std::string_view privateKeyStr);
 std::string GetAddressFromPrivateKey(std::string_view privateKeyStr);
