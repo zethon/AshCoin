ceab6e654ca53245c83302bcb7df1d2b92c4ca99
Adalid Claure <aclaure@gmail.com>
2021-03-11 09:07:10 -0500

    starting to clean up web interface;

--- src/html/common.js
+++ src/html/common.js
@@ -1,1 +1,25 @@
 var ServerUrl = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+
+function RequestShutdown()
+{
+    var shutdownHttp = new XMLHttpRequest();
+    shutdownHttp.open("GET", `${ServerUrl}/rest/shutdown`);
+    shutdownHttp.send();
+    shutdownHttp.onreadystatechange = (e) => { };
+}
+
+function RequestMiningStart(readyCallback)
+{
+    Http = new XMLHttpRequest();
+    Http.open("GET", ServerUrl+"/rest/startMining");
+    Http.onreadystatechange = readyCallback;
+    return Http;
+}
+
+function RequestMiningStop(readyCallback)
+{
+    Http = new XMLHttpRequest();
+    Http.open("GET", ServerUrl+"/rest/stopMining");
+    Http.onreadystatechange = readyCallback;
+    Http.send();
+}
