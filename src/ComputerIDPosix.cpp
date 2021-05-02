bcff9e7a473bb51a291f6a4f223cf3bbb54437d0
Addy <aclaure@gmail.com>
2021-05-02 18:43:49 -0400

    a better macos computer id

--- src/ComputerIDPosix.cpp
+++ src/ComputerIDPosix.cpp
@@ -1,5 +1,10 @@
-#include <boost/asio.hpp>
 #include <iostream>
+#include <ifaddrs.h>
+#include <sys/sysctl.h>
+#include <net/if_dl.h>
+#include <net/if_types.h>
+
+#include <boost/asio.hpp>
 
 #include <cryptopp/sha.h>
 #include <cryptopp/filters.h>
@@ -10,10 +15,10 @@
 namespace utils
 {
 
-std::string SHA256(std::string data)
+std::string SHA1(const std::string& data)
 {
     std::string digest;
-    CryptoPP::SHA256 hash;
+    CryptoPP::SHA1 hash;
 
     CryptoPP::StringSource foo(data, true,
         new CryptoPP::HashFilter(hash,
@@ -23,17 +28,102 @@ std::string SHA256(std::string data)
     return digest;
 }
 
-// eventually this should get the mac address and do 
-// some other cool stuff, but for now we'll just use
-// a hash of the computer's hostname and a pointer
-// to *this
+std::string getMacString()
+{
+    struct ifaddrs *ifaddr;
+    int family, s;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1)
+    {
+        throw std::runtime_error("call to getifaddrs failed");
+    }
+
+    int mib[6];
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+
+    std::size_t len;
+    int idx = 0;
+    char *buf;
+
+    struct if_msghdr    *ifm;
+    struct sockaddr_dl  *sdl;
+    unsigned char       *ptr;
+
+    std::stringstream   builder;
+
+    while (ifaddr != nullptr)
+    {
+        if (ifaddr->ifa_addr != nullptr)
+        {
+            auto family = ifaddr->ifa_addr->sa_family;
+
+            if ((mib[5] = if_nametoindex(ifaddr->ifa_name)) == 0)
+            {
+                continue;
+            }
+
+            if (sysctl(mib, 6, nullptr, &len, nullptr, 0) < 0)
+            {
+                continue;
+            }
+
+            if ((buf = static_cast<char*>(malloc(len))) == nullptr)
+            {
+                continue;
+            }
+
+            if (sysctl(mib, 6, buf, &len, nullptr, 0) < 0)
+            {
+                continue;
+            }
+
+            ifm = (struct if_msghdr *)buf;
+            sdl = (struct sockaddr_dl *)(ifm + 1);
+
+            if (sdl->sdl_type == IFT_ETHER)
+            {
+                ptr = (unsigned char *)LLADDR(sdl);
+
+                builder
+                    << std::hex
+                    << static_cast<int>(*ptr)
+                    << ':'
+                    << static_cast<int>(*(ptr + 1))
+                    << ':'
+                    << static_cast<int>(*(ptr + 2))
+                    << ':'
+                    << static_cast<int>(*(ptr + 3))
+                    << ':'
+                    << static_cast<int>(*(ptr + 4))
+                    << ':'
+                    << static_cast<int>(*(ptr + 5));
+            }
+
+            free(buf);
+        }
+
+        ++idx;
+        ifaddr = ifaddr->ifa_next;
+    }
+
+    return builder.str();
+}
+
+ComputerID::ComputerID() = default;
+
 std::string ComputerID::getUUID()
 {
     namespace ip = boost::asio::ip;
     boost::asio::io_service io_service;
 
     std::stringstream ss;
-    ss << ip::host_name();
+    ss << ip::host_name()
+        << getMacString();
     if (_uniquePerProcess)
     {
         ss << static_cast<void*>(this);
@@ -44,7 +134,7 @@ std::string ComputerID::getUUID()
         ss << _customData;
     }
 
-    return SHA256(ss.str());
+    return SHA1(ss.str());
 }
 
 } // namespace utils
